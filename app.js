d9e1563b02c1a489ae9f75b2cab021a48ee80c8c
Naveed <08es34@gmail.com>
2020-04-17 17:35:36 +0500

    added action buttons

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ Vue.component('textElement', {
             default: () => ({
                 text: '',
                 fontSize: '12px',
-                backgroundColor: '#fff',
-                color: '#000',
+                backgroundColor: '#ffffff',
+                color: '#000000',
             }),
         }
     },
@@ -18,30 +18,82 @@ Vue.component('textElement', {
         }
     },
     template: `
-    <span :style="style" class="text-element">{{ data.text }}</span>
+    <span :style="style" class="text-element" @mouseup="$emit('click', $event)">{{ data.text }}</span>
   `,
+});
+
+Vue.component('editor', {
+    props: {
+        data: Array
+    },
+    template: `
+    <div class="editor" spellcheck="false" contenteditable="true" ref="editor" @mouseup="$emit('mouseup', $event)">
+        <text-element v-for="(item, index) in data" :key="index" :data="item" @click="$emit('selected', item)"></text-element>
+    </div>
+  `,
+    mounted() {
+        this.$refs.editor.focus();
+    }
+});
+
+Vue.component('actionButton', {
+    props: {
+        name: String,
+        value: String,
+    },
+    methods: {
+        perform() {
+            let value = '';
+            switch (this.name) {
+                case 'inc-size':
+                    value = parseInt(this.value);
+                    this.$emit('input', (value + 2) + 'px');
+                    break;
+                case 'dec-size':
+                    value = parseInt(this.value);
+                    this.$emit('input', (value - 2) + 'px');
+                    break;
+                case 'change-bkg-color':
+                case 'change-color':
+                    value = this.value;
+                    this.$refs.colorInput.value = value;
+                    this.$refs.colorInput.click();
+                    break;
+                default:
+                    console.log('Here we are');
+            }
+        }
+    },
+    template: `
+    <div :class="'button ' + name" @click="perform">
+        <div class="inputs" style="position:absolute; opacity: 0">
+              <input type="color" ref="colorInput" @input="$emit('input', $event.target.value)">  
+        </div>
+        <slot></slot>
+    </div>
+  `
 });
 
 let app = new Vue({
     el: "#app",
     data: {
-        text: [
+        textItems: [
             {
                 text: 'Hi,\n',
                 fontSize: '32px',
-                backgroundColor: 'rgb(248, 187, 208)',
-                color: '#fff',
+                backgroundColor: '#F8BBD0',
+                color: '#ffffff',
             },
             {
                 text: 'My lovely ',
                 fontSize: '32px',
-                backgroundColor: 'rgb(248, 187, 208)',
-                color: '#fff',
+                backgroundColor: '#F8BBD0',
+                color: '#ffffff',
             },
             {
                 text: 'little ',
                 fontSize: '54px',
-                backgroundColor: 'white',
+                backgroundColor: '#ffffff',
                 color: 'rgb(136, 14, 79)',
             },
             {
@@ -51,10 +103,24 @@ let app = new Vue({
                 backgroundColor: 'rgb(248, 187, 0)'
             },
         ],
+        selectedItem: null,
+        selectedText: '',
     },
     methods: {
-        remove(item) {
-            this.items = _.reject(this.items, i => item.id === i.id);
+        onTextSelected(item) {
+            this.selectedItem = item;
+            this.selectedText = this.getSelectedText();
         },
+        getSelectedText() {
+            if (window.getSelection) {
+                return window.getSelection().toString();
+            } else if (document.selection) {
+                return document.selection.createRange().text;
+            }
+            return '';
+        }
+    },
+    mounted() {
+        this.selectedItem = this.textItems[0];
     }
 });
